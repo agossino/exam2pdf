16c0cef5cbab2172cf42893ffcf39b025e0046c9
Giancarlo Ossino <gcossino@gmail.com>
2020-07-05 17:36:54 +0200

    project renamed to exam2pdf; in github it can coexist

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import find_packages, setup
-import quest2pdf
+import exam2pdf
 
 with open("README.md", "r") as fd:
     long_description = fd.read()
 
 setup(
-   name="quest2pdf",
-   description="From cvs to question.",
+   name="exam2pdf",
+   description="From cvs to exam.",
    long_description=long_description,
    license="License :: OSI Approved :: MIT License",
    author="Giancarlo Ossino",
    author_email="gcossino@gmail.com",
-   url="https://github.com/agossino/quest2pdf",
-   version=quest2pdf.__version__,
+   url="https://github.com/agossino/exam2pdf",
+   version=exam2pdf.__version__,
    packages=find_packages(),
    python_requires=">=3.6"
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,16 @@ import gettext
 
 import pytest
 
-import quest2pdf
+import exam2pdf
 
 
 @pytest.fixture
 def set_i18n(tmp_path):
-    domain = "quest2pdf"
-    mo_file = "quest2pdf.mo"
+    domain = "exam2pdf"
+    mo_file = "exam2pdf.mo"
     locale_dir = "locales"
     it_dir = "it/LC_MESSAGES"
-    app_locales_dir = Path("quest2pdf") / locale_dir
+    app_locales_dir = Path("exam2pdf") / locale_dir
     mo_source_path = app_locales_dir / it_dir / mo_file
     file_data = mo_source_path.read_bytes()
 
@@ -33,29 +33,29 @@ def set_i18n(tmp_path):
 
 @pytest.fixture
 def question1():
-    return quest2pdf.Question("q1 text", "q1 image")
+    return exam2pdf.Question("q1 text", "q1 image")
 
 
 @pytest.fixture
 def question2():
-    return quest2pdf.Question("q2 text", "q2 image"),
+    return exam2pdf.Question("q2 text", "q2 image"),
 
 
 @pytest.fixture
 def dummy_exam():
     q1, q2, q3, q4, q5 = (
-        quest2pdf.Question("q1 text", "q1 subject", Path("a.png"), 1),
-        quest2pdf.Question("q2 text", "q2 subject", Path("b.png"), 2),
-        quest2pdf.Question("q3 text", "q3 subject", Path("c.png"), 3),
-        quest2pdf.Question("q4 text", "q4 subject", Path("a.png"), 4),
-        quest2pdf.Question("q5 text", "q5 subject", Path("b.png"), 5),
+        exam2pdf.Question("q1 text", "q1 subject", Path("a.png"), 1),
+        exam2pdf.Question("q2 text", "q2 subject", Path("b.png"), 2),
+        exam2pdf.Question("q3 text", "q3 subject", Path("c.png"), 3),
+        exam2pdf.Question("q4 text", "q4 subject", Path("a.png"), 4),
+        exam2pdf.Question("q5 text", "q5 subject", Path("b.png"), 5),
     )
 
-    q1.answers = (quest2pdf.Answer("q1 a1"), quest2pdf.Answer("q1 a2"))
+    q1.answers = (exam2pdf.Answer("q1 a1"), exam2pdf.Answer("q1 a2"))
     q1.correct_index = 1
-    q2.answers = (quest2pdf.Answer("q2 a1"), quest2pdf.Answer("q2 a2"))
+    q2.answers = (exam2pdf.Answer("q2 a1"), exam2pdf.Answer("q2 a2"))
 
-    return quest2pdf.Exam(q1, q2, q3, q4, q5)
+    return exam2pdf.Exam(q1, q2, q3, q4, q5)
 
 
 @pytest.fixture
@@ -76,82 +76,82 @@ def dummy_exam_with_img(tmp_path, dummy_exam):
 
 @pytest.fixture
 def mix_dummy_exam():
-    q1 = quest2pdf.Question("question 1: correct is n. 2", "subject 1", Path("a.png"))
-    a1 = quest2pdf.Answer("answer 1", Path("b.png"))
-    a2 = quest2pdf.Answer("answer 2", Path("c.png"))
-    a3 = quest2pdf.Answer("answer 3", Path("a.png"))
+    q1 = exam2pdf.Question("question 1: correct is n. 2", "subject 1", Path("a.png"))
+    a1 = exam2pdf.Answer("answer 1", Path("b.png"))
+    a2 = exam2pdf.Answer("answer 2", Path("c.png"))
+    a3 = exam2pdf.Answer("answer 3", Path("a.png"))
     q1.answers = (a1, a2, a3)
     q1.correct_option = "B"
 
-    q2 = quest2pdf.Question("question 2: correct is n. 1", "subject 1", Path("a.png"))
-    a1 = quest2pdf.Answer("answer 1")
-    a2 = quest2pdf.Answer("answer 2")
-    a3 = quest2pdf.Answer("answer 3")
+    q2 = exam2pdf.Question("question 2: correct is n. 1", "subject 1", Path("a.png"))
+    a1 = exam2pdf.Answer("answer 1")
+    a2 = exam2pdf.Answer("answer 2")
+    a3 = exam2pdf.Answer("answer 3")
     q2.answers = (a1, a2, a3)
 
-    q3 = quest2pdf.TrueFalseQuest("question 3: correct is True (first)")
-    a1 = quest2pdf.TrueFalseAnswer(True)
-    a2 = quest2pdf.TrueFalseAnswer(False)
+    q3 = exam2pdf.TrueFalseQuest("question 3: correct is True (first)")
+    a1 = exam2pdf.TrueFalseAnswer(True)
+    a2 = exam2pdf.TrueFalseAnswer(False)
     q3.answers = (a1, a2)
 
-    q4 = quest2pdf.Question("question 4: no answer", "subject 2", Path("b.png"))
+    q4 = exam2pdf.Question("question 4: no answer", "subject 2", Path("b.png"))
 
-    q5 = quest2pdf.TrueFalseQuest("question 5: correct is False (first))")
-    a1 = quest2pdf.TrueFalseAnswer(False)
-    a2 = quest2pdf.TrueFalseAnswer(True)
+    q5 = exam2pdf.TrueFalseQuest("question 5: correct is False (first))")
+    a1 = exam2pdf.TrueFalseAnswer(False)
+    a2 = exam2pdf.TrueFalseAnswer(True)
     q5.answers = (a1, a2)
 
-    q6 = quest2pdf.Question("question 6: correct is n. 3", "subject 4", Path("c.png"))
-    a1 = quest2pdf.Answer("answer 1")
-    a2 = quest2pdf.Answer("answer 2")
-    a3 = quest2pdf.Answer("answer 3")
-    a4 = quest2pdf.Answer("answer 4")
+    q6 = exam2pdf.Question("question 6: correct is n. 3", "subject 4", Path("c.png"))
+    a1 = exam2pdf.Answer("answer 1")
+    a2 = exam2pdf.Answer("answer 2")
+    a3 = exam2pdf.Answer("answer 3")
+    a4 = exam2pdf.Answer("answer 4")
     q6.add_answer(a1)
     q6.add_answer(a2)
     q6.add_answer(a3, is_correct=True)
     q6.add_answer(a4)
-    dummy_ex = quest2pdf.Exam(q1, q2, q3, q4, q5, q6)
+    dummy_ex = exam2pdf.Exam(q1, q2, q3, q4, q5, q6)
 
     return dummy_ex
 
 
 @pytest.fixture
 def big_dummy_exam():
-    q1 = quest2pdf.Question("question 1: correct is n. 2", "subject 1", Path("a.png"))
-    a1 = quest2pdf.Answer("answer 1", Path("b.png"))
-    a2 = quest2pdf.Answer("answer 2", Path("c.png"))
-    a3 = quest2pdf.Answer("answer 3", Path("a.png"))
+    q1 = exam2pdf.Question("question 1: correct is n. 2", "subject 1", Path("a.png"))
+    a1 = exam2pdf.Answer("answer 1", Path("b.png"))
+    a2 = exam2pdf.Answer("answer 2", Path("c.png"))
+    a3 = exam2pdf.Answer("answer 3", Path("a.png"))
     q1.answers = (a1, a2, a3)
     q1.correct_option = "B"
 
-    q2 = quest2pdf.Question("question 2: correct is n. 1", "subject 1", Path("a.png"))
-    a1 = quest2pdf.Answer("answer 1")
-    a2 = quest2pdf.Answer("answer 2")
-    a3 = quest2pdf.Answer("answer 3")
+    q2 = exam2pdf.Question("question 2: correct is n. 1", "subject 1", Path("a.png"))
+    a1 = exam2pdf.Answer("answer 1")
+    a2 = exam2pdf.Answer("answer 2")
+    a3 = exam2pdf.Answer("answer 3")
     q2.answers = (a1, a2, a3)
 
-    q3 = quest2pdf.TrueFalseQuest("question 3: correct is True (first)")
-    a1 = quest2pdf.TrueFalseAnswer(True)
-    a2 = quest2pdf.TrueFalseAnswer(False)
+    q3 = exam2pdf.TrueFalseQuest("question 3: correct is True (first)")
+    a1 = exam2pdf.TrueFalseAnswer(True)
+    a2 = exam2pdf.TrueFalseAnswer(False)
     q3.answers = (a1, a2)
 
-    q4 = quest2pdf.Question("question 4: no answer", "subject 2", Path("b.png"))
+    q4 = exam2pdf.Question("question 4: no answer", "subject 2", Path("b.png"))
 
-    q5 = quest2pdf.TrueFalseQuest("question 5: correct is False (first))")
-    a1 = quest2pdf.TrueFalseAnswer(False)
-    a2 = quest2pdf.TrueFalseAnswer(True)
+    q5 = exam2pdf.TrueFalseQuest("question 5: correct is False (first))")
+    a1 = exam2pdf.TrueFalseAnswer(False)
+    a2 = exam2pdf.TrueFalseAnswer(True)
     q5.answers = (a1, a2)
 
-    q6 = quest2pdf.Question("question 6: correct is n. 3", "subject 4", Path("c.png"))
-    a1 = quest2pdf.Answer("answer 1")
-    a2 = quest2pdf.Answer("answer 2")
-    a3 = quest2pdf.Answer("answer 3")
-    a4 = quest2pdf.Answer("answer 4")
+    q6 = exam2pdf.Question("question 6: correct is n. 3", "subject 4", Path("c.png"))
+    a1 = exam2pdf.Answer("answer 1")
+    a2 = exam2pdf.Answer("answer 2")
+    a3 = exam2pdf.Answer("answer 3")
+    a4 = exam2pdf.Answer("answer 4")
     q6.add_answer(a1)
     q6.add_answer(a2)
     q6.add_answer(a3, is_correct=True)
     q6.add_answer(a4)
-    dummy_ex = quest2pdf.Exam(q1, q2, q3, q4, q5, q6)
+    dummy_ex = exam2pdf.Exam(q1, q2, q3, q4, q5, q6)
 
     return dummy_ex
 
