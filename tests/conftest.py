0d147dec2237a1fa911c386c8d420638a77eb4ff
Giancarlo Ossino <gcossino@gmail.com>
2020-07-16 21:59:17 +0200

    Added some tests for missing images exception; restructured some tests; removed i18n from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,12 @@
+from itertools import chain
 import os
 from pathlib import Path
-from itertools import chain
 import random
 import subprocess
-import gettext
 
 import pytest
 
 import exam2pdf
-
-
-@pytest.fixture
-def set_i18n(tmp_path):
-    domain = "exam2pdf"
-    mo_file = "exam2pdf.mo"
-    locale_dir = "locales"
-    it_dir = "it/LC_MESSAGES"
-    app_locales_dir = Path("exam2pdf") / locale_dir
-    mo_source_path = app_locales_dir / it_dir / mo_file
-    file_data = mo_source_path.read_bytes()
-
-    tmp_locales = tmp_path / locale_dir
-    destination_dir = tmp_locales / it_dir
-    destination_dir.mkdir(parents=True)
-    mo_destination_file = destination_dir / mo_file
-    mo_destination_file.write_bytes(file_data)
-
-    gettext.bindtextdomain(domain, localedir=tmp_locales)
-
-    yield
 
 
 @pytest.fixture
@@ -51,20 +29,27 @@ def dummy_exam():
         exam2pdf.Question("q5 text", "q5 subject", Path("b.png"), 5),
     )
 
-    q1.answers = (exam2pdf.Answer("q1 a1"), exam2pdf.Answer("q1 a2"))
+    q1.answers = (exam2pdf.Answer("q1 a1"), exam2pdf.Answer("q1 a2", Path("t1.jpg")))
     q1.correct_index = 1
-    q2.answers = (exam2pdf.Answer("q2 a1"), exam2pdf.Answer("q2 a2"))
+    q2.answers = (exam2pdf.Answer("q2 a1", Path("t2.png")), exam2pdf.Answer("q2 a2"))
 
     return exam2pdf.Exam(q1, q2, q3, q4, q5)
 
 
 @pytest.fixture
-def dummy_exam_with_img(tmp_path, dummy_exam):
+def tmp_folders(tmp_path):
     image_folder = Path("tests/unit/resources")
     image_tmp_folder = tmp_path / image_folder.name
     image_tmp_folder.mkdir()
 
-    for file in chain(image_folder.glob("*.png"), image_folder.glob("*.jpg")):
+    return image_folder, image_tmp_folder
+
+
+@pytest.fixture
+def dummy_exam_questions_without_img(tmp_path, tmp_folders, dummy_exam):
+    image_folder, image_tmp_folder = tmp_folders
+
+    for file in (image_folder / "t1.jpg", image_folder / "t2.png"):
         data = file.read_bytes()
         copied_file = tmp_path / image_folder.name / file.name
         copied_file.write_bytes(data)
@@ -75,56 +60,34 @@ def dummy_exam_with_img(tmp_path, dummy_exam):
 
 
 @pytest.fixture
-def mix_dummy_exam():
-    q1 = exam2pdf.Question("question 1: correct is n. 2", "subject 1", Path("a.png"))
-    a1 = exam2pdf.Answer("answer 1", Path("b.png"))
-    a2 = exam2pdf.Answer("answer 2", Path("c.png"))
-    a3 = exam2pdf.Answer("answer 3", Path("a.png"))
-    q1.answers = (a1, a2, a3)
-    q1.correct_option = "B"
+def dummy_exam_answers_without_img(tmp_path, tmp_folders, dummy_exam):
+    image_folder, image_tmp_folder = tmp_folders
 
-    q2 = exam2pdf.Question("question 2: correct is n. 1", "subject 1", Path("a.png"))
-    a1 = exam2pdf.Answer("answer 1")
-    a2 = exam2pdf.Answer("answer 2")
-    a3 = exam2pdf.Answer("answer 3")
-    q2.answers = (a1, a2, a3)
+    for file in chain(image_folder.glob("?.png")):
+        data = file.read_bytes()
+        copied_file = tmp_path / image_folder.name / file.name
+        copied_file.write_bytes(data)
 
-    q3 = exam2pdf.TrueFalseQuest("question 3: correct is True (first)")
-    a1 = exam2pdf.TrueFalseAnswer(True)
-    a2 = exam2pdf.TrueFalseAnswer(False)
-    q3.answers = (a1, a2)
+    dummy_exam.add_path_parent(image_tmp_folder)
 
-    q4 = exam2pdf.Question("question 4: no answer", "subject 2", Path("b.png"))
-
-    q5 = exam2pdf.TrueFalseQuest("question 5: correct is False (first))")
-    a1 = exam2pdf.TrueFalseAnswer(False)
-    a2 = exam2pdf.TrueFalseAnswer(True)
-    q5.answers = (a1, a2)
-
-    q6 = exam2pdf.Question("question 6: correct is n. 3", "subject 4", Path("c.png"))
-    a1 = exam2pdf.Answer("answer 1")
-    a2 = exam2pdf.Answer("answer 2")
-    a3 = exam2pdf.Answer("answer 3")
-    a4 = exam2pdf.Answer("answer 4")
-    q6.add_answer(a1)
-    q6.add_answer(a2)
-    q6.add_answer(a3, is_correct=True)
-    q6.add_answer(a4)
-    dummy_ex = exam2pdf.Exam(q1, q2, q3, q4, q5, q6)
-
-    return dummy_ex
+    return dummy_exam
 
 
 @pytest.fixture
-def big_dummy_exam():
-    q1 = exam2pdf.Question("question 1: correct is n. 2", "subject 1", Path("a.png"))
+def dummy_exam_with_img(dummy_exam, dummy_exam_questions_without_img, dummy_exam_answers_without_img):
+    return dummy_exam
+
+
+@pytest.fixture
+def mix_dummy_exam():
+    q1 = exam2pdf.Question("question 1: correct is n. two", "subject 1", Path("a.png"))
     a1 = exam2pdf.Answer("answer 1", Path("b.png"))
     a2 = exam2pdf.Answer("answer 2", Path("c.png"))
     a3 = exam2pdf.Answer("answer 3", Path("a.png"))
     q1.answers = (a1, a2, a3)
     q1.correct_option = "B"
 
-    q2 = exam2pdf.Question("question 2: correct is n. 1", "subject 1", Path("a.png"))
+    q2 = exam2pdf.Question("question 2: correct is n. one", "subject 1", Path("a.png"))
     a1 = exam2pdf.Answer("answer 1")
     a2 = exam2pdf.Answer("answer 2")
     a3 = exam2pdf.Answer("answer 3")
@@ -142,7 +105,7 @@ def big_dummy_exam():
     a2 = exam2pdf.TrueFalseAnswer(True)
     q5.answers = (a1, a2)
 
-    q6 = exam2pdf.Question("question 6: correct is n. 3", "subject 4", Path("c.png"))
+    q6 = exam2pdf.Question("question 6: correct is n. three", "subject 4", Path("c.png"))
     a1 = exam2pdf.Answer("answer 1")
     a2 = exam2pdf.Answer("answer 2")
     a3 = exam2pdf.Answer("answer 3")
@@ -163,41 +126,6 @@ def save_app_configuration(file_path):
 
 def save_app_configuration_set(file_path):
     text = "[Default]\nnumber = 3\nexam = Exam from conf.ini\n"
-    file_path.write_text(text)
-
-
-def save_log_configuration(file_path):
-    text = """{
-    "disable_existing_loggers": false,
-    "formatters": {
-        "default": {
-            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        }
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "default",
-            "stream": "ext://sys.stdout"
-        },
-        "file": {
-             "level": "DEBUG",
-             "class": "logging.handlers.RotatingFileHandler",
-             "formatter": "default",
-             "filename": "application.log",
-             "maxBytes": 1024000,
-             "backupCount": 3
-        }
-    },
-    "loggers": {
-        "": {
-            "handlers": ["console"],
-            "level": "DEBUG",
-            "propagate": true
-        }
-    },
-    "version": 1
-}"""
     file_path.write_text(text)
 
 
@@ -260,7 +188,7 @@ def weired_csv_file(tmp_path):
 
 
 @pytest.fixture
-def have_a_look(tmp_path, big_dummy_exam):
+def have_a_look(tmp_path, mix_dummy_exam):
     image_folder = Path("tests/unit/resources")
     image_tmp_folder = tmp_path / image_folder.name
     image_tmp_folder.mkdir()
@@ -273,7 +201,7 @@ def have_a_look(tmp_path, big_dummy_exam):
 
     exam_file_path = tmp_path / "Exam"
     correction_file_path = tmp_path / "Correction"
-    ex = big_dummy_exam
+    ex = mix_dummy_exam
     ex.add_path_parent(image_tmp_folder)
     ex.print(exam_file_path,
              correction_file_name=correction_file_path,
